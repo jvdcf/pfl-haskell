f07ab8e50401a522bdafb12625ff1f64d2c06d2b
jvdcf <up202208393@up.pt>
2024-10-14 08:10:26 +0100

    Refactor project file and Cities function

--- TP1.hs
+++ TP1.hs
@@ -1,21 +1,30 @@
---import qualified Data.List
---import qualified Data.Array
---import qualified Data.Bits
+import qualified Data.List
+import qualified Data.Array
+import qualified Data.Bits
 
--- PFL 2024/2025 Practical assignment 1
+{- TP1: Haskell Coursework
+   Programação Funcional e em Lógica (L.EIC024) 2024/2025
+   Licenciatura em Engenharia Informática e Computação
+   António Mário da Silva Marcos Florido (Regente e professor das aulas práticas)
 
-main = undefined
+   Grupo T02_GXX
+   - Guilherme Duarte Silva Matos (up202208755@up.pt)
+   - João Vítor da Costa Ferreira (up202208393@up.pt)
+ -}
 
--- Uncomment the some/all of the first three lines to import the modules, do not change the code of these lines.
+-- Data structures -----------------------------------------
 
 type City = String
 type Path = [City]
 type Distance = Int
 
-type RoadMap = [(City,City,Distance)]
+type RoadMap = [(City,City,Distance)] -- Adjacency list 
+
+-- Functions implemented -----------------------------------
 
 cities :: RoadMap -> [City]
-cities = undefined -- modifiy this line to implement the solution, for each exercise not solved, leave the function definition like this
+cities rm = rmDupl ([ s | (s, _, _) <- rm] ++ [ t | (_, t, _) <- rm])
+    where rmDupl = map head . Data.List.group . Data.List.sort
 
 areAdjacent :: RoadMap -> City -> City -> Bool
 areAdjacent = undefined
@@ -41,10 +50,13 @@ shortestPath = undefined
 travelSales :: RoadMap -> Path
 travelSales = undefined
 
+-- Functions not applicable to our group of 2 people -------
+
 tspBruteForce :: RoadMap -> Path
 tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
 
--- Some graphs to test your work
+-- Some graphs for testing ---------------------------------
+
 gTest1 :: RoadMap
 gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
 
