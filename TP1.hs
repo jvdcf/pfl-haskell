7b4c8c3e05bc1378b777c2fa391b19a53f30c7e6
jvdcf <up202208393@up.pt>
2024-11-01 09:32:50 +0000

    Better formatting

--- TP1.hs
+++ TP1.hs
@@ -1,7 +1,7 @@
 import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
-import Debug.Trace
+import Debug.Trace ()
 
 {- TP1: Haskell Coursework
    Programação Funcional e em Lógica (L.EIC024) 2024/2025
@@ -13,7 +13,9 @@ import Debug.Trace
    - João Vítor da Costa Ferreira (up202208393@up.pt)
 -}
 
--- Data structures -----------------------------------------
+
+
+-- Data structures ------------------------------------------------------------------------------
 
 type City = String  -- Node of a graph
 type Path = [City]  -- A list of nodes representing a path
@@ -22,7 +24,8 @@ type Distance = Int -- Weight of an edge
 -- An adjacency list representation of the edges in a undirected and weighted graph
 type RoadMap = [(City,City,Distance)]
 
--- Project convention --------------------------------------
+
+-- Project convention ---------------------------------------------------------------------------
 
 -- s  -> Source node/city
 -- t  -> Sink or Destination node/city
@@ -30,17 +33,21 @@ type RoadMap = [(City,City,Distance)]
 -- c  -> City
 -- d  -> Distance
 
--- Functions implemented -----------------------------------
+
+
+-- Functions implemented ------------------------------------------------------------------------
 
 {- Returns all the cities in the graph. -}
 cities :: RoadMap -> [City]
 cities rm = rmDupl ([ s | (s, _, _) <- rm] ++ [ t | (_, t, _) <- rm])
   where rmDupl = map head . Data.List.group . Data.List.sort
 
+
 {- Returns a boolean indicating whether two cities are linked directly. -}
 areAdjacent :: RoadMap -> City -> City -> Bool
 areAdjacent rm c1 c2 = length finds > 0
   where finds = [ True | (s, t, d) <- rm, s == c1, t == c2]
+
 
 {- Returns a Just value with the distance between two cities connected directly, given two city names, and Nothing otherwise. -}
 distance :: RoadMap -> City -> City -> Maybe Distance
@@ -48,11 +55,13 @@ distance rm c1 c2 | length finds > 0 = Just (head finds)
                   | otherwise = Nothing
                   where finds = [ d | (s, t, d) <- rm, s == c1, t == c2]
 
+
 {- Returns the cities adjacent to a particular city (i.e. cities with a direct edge between them) and the respective distances to them. -}
 adjacent :: RoadMap -> City -> [(City,Distance)]
 adjacent rm c = sinks ++ sources
   where sinks = [(t, d) | (s, t, d) <- rm, s == c]
         sources = [(s, d) | (s, t, d) <- rm, t == c]
+
 
 {- Returns the sum of all individual distances in a path between two cities in a Just value, if all the consecutive pairs of cities are directly connected by roads. Otherwise, it returns a Nothing. -}
 pathDistance :: RoadMap -> Path -> Maybe Distance
@@ -63,6 +72,7 @@ pathDistance rm (s:t:ps) =
   pathDistance rm (t:ps) >>= \rest ->
   return (this + rest)
 
+
 {- Returns the names of the cities with the highest number of roads connecting to them (i.e. the vertices with the highest degree). -}
 rome :: RoadMap -> [City]
 rome [] = []
@@ -70,6 +80,7 @@ rome rm = filter f (cities rm)
   where f c = degree c == maxDegree rm
         maxDegree rm = maximum [ degree c | c <- cities rm]
         degree c = length (adjacent rm c)
+
 
 {- Returns a boolean indicating whether all the cities in the graph are connected in the roadmap (i.e., if every city is reachable from every other city). -}
 isStronglyConnected :: RoadMap -> Bool
@@ -82,23 +93,29 @@ isStronglyConnected rm = length (dfsVisit rm startCity) == length (cities rm)
                 subsetRoadMap  = filter (\(s, t, d) -> s /= c && t /= c) rm
                 nextCity       = fst $ head $ adjacent rm c
 
+
 {- Computes all shortest paths connecting the two cities given as input. Note that there may be more than one path with the same total distance. If there are no paths between the input cities, then return an empty list. Note that the (only) shortest path between a city c and itself is [c]. -}
 shortestPath :: RoadMap -> City -> City -> [Path]
 shortestPath [] _ _ = []
 shortestPath _ c1 c2 | c1 == c2 = [[c1]]
 shortestPath rm c1 c2 = undefined -- TODO (Recommended to implement Dijkstra's algorithm)
 
+
 {- Given a roadmap, returns a solution of the Traveling Salesman Problem (TSP): visit each city exactly once and come back to the starting town in the route whose total distance is minimum. Any optimal TSP path will be accepted and the function only needs to return one of them, so the starting city (which is also the ending city) is left to be chosen by each group. If the graph does not have a TSP path, then return an empty list. -}
 travelSales :: RoadMap -> Path
 travelSales [] = []
 travelSales rm = undefined -- TODO (Recommended to implement using dynamic programming)
 
--- Functions not applicable to our group of 2 people -------
+
+
+-- Functions not applicable to our group of 2 people --------------------------------------------
 
 tspBruteForce :: RoadMap -> Path
 tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
 
--- Some graphs for testing ---------------------------------
+
+
+-- Some graphs for testing ----------------------------------------------------------------------
 
 gTest1 :: RoadMap
 gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
